e1b149131190e8c8c8e10412b2af17cd485354d2
Yam Sol Bertamini <yambertamini.sol@gmail.com>
2023-06-27 19:11:30 -0300

    corpo do programa. chama as funcoes e recebe input

---
+++ key_generation/main.py
@@ -0,0 +1,21 @@
+from gerar_par_de_chaves import gerar_keys
+from MsgHandler import msg_encrypt, msg_decrypt
+
+# Titulo
+print('*'*15, "Sistema de Mensagens", '*'*15)
+
+# Gerando chaves
+a, b = gerar_keys()
+pub_key = {'e': a[0], 'n': a[1]}
+priv_key = {'d': b[0], 'n': b[1]}
+
+# Recebendo mensagem
+message = input("Digite sua mensagem: ")
+
+# Encriptando mensagem
+encrypted_menssage = msg_encrypt(message,pub_key)
+print(encrypted_menssage)
+
+# Desencriptando mensagem
+decrypted_menssage = msg_decrypt(encrypted_menssage,priv_key)
+print(decrypted_menssage)
